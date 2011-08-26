894e5eab89251890c9ea0da15f7cc0f9a8deea1a
Peter Shih <ptshih@gmail.com>
2011-08-26 15:08:37 -0700

    headers

--- PhotoTime/CommentView.h
+++ PhotoTime/CommentView.h
@@ -3,7 +3,7 @@
 //  PhotoTime
 //
 //  Created by Peter Shih on 8/8/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Seven Minute Labs. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
